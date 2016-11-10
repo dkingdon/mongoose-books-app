e6ee1677262d788441d74b19c26c5a912f7837f1
dkingdon <daniel.kingdon@gmail.com>
2016-11-09 20:54:17 -0800

    completed set up steps

---
+++ models/book.js
@@ -0,0 +1,14 @@
+// entire book.js so far
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var BookSchema = new Schema({
+  title: String,
+  author: String,
+  image: String,
+  releaseDate: String
+});
+
+var Book = mongoose.model('Book', BookSchema);
+
+module.exports = Book;
